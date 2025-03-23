b33058eb730f51fd4006b760a1d8deb5704b604d
achance6 <ayden.chance@rocketmail.com>
2025-03-23 18:13:40 -0400

    Update and export video interface

--- src/assets/sampleVideos.ts
+++ src/assets/sampleVideos.ts
@@ -1,38 +1,42 @@
 import sampleThumbnail from '@/assets/sample-thumbnail.jpg'
 
-interface Video {
+export interface Video {
     title: string,
+    videoSrc: string,
+    uploader: string,
     channelName: string,
     views: string,
     time: string,
-    thumbnail: string
+    thumbnail: string,
+    tags: string[]
 }
 
 const sampleVideos: Video[] = [{
-    title: "Title 1", channelName: "Channel 1", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 1", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 1", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 2", channelName: "Channel 2", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 2", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 2", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 3", channelName: "Channel 3", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 3", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 3", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 4", channelName: "Channel 4", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 4", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 4", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 5", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 5", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 5", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 5", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 5", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 5", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 5", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 5", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 5", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 5", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 5", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 5", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 5", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 5", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 5", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 5", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }, {
-    title: "Title 5", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail
+    title: "Title 5", videoSrc: "video.mp4", uploader: "test@test.com", channelName: "Channel 5", views: "100", time: "1 day ago", thumbnail: sampleThumbnail, tags: []
 }];
+
 export default sampleVideos;
