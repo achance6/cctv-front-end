608c40ea1b56e77b7ffe8c3413204c46fd1535ad
achance6 <ayden.chance@rocketmail.com>
2025-04-01 12:52:24 -0400

    Video card / home page styling

--- src/components/videoCard.tsx
+++ src/components/videoCard.tsx
@@ -1,5 +1,6 @@
 import "@/assets/css/videoCard.css";
 import { Card, Flex, Image, Text, View } from "@aws-amplify/ui-react";
+import { Link } from "react-router";
 
 interface VideoCardProps {
   thumbnail: string;
@@ -11,41 +12,44 @@ interface VideoCardProps {
 
 function VideoCard(props: VideoCardProps) {
   return (
-    <Card
-      className="videoCard"
-      variation="outlined"
-      maxWidth="350px"
-      maxHeight="250px"
-    >
-      <Image
-        className="videoThumbnail"
-        src={props.thumbnail}
-        alt={props.title}
-      />
-      <View>
-        <Text
-          isTruncated={true}
-          className="videoTitle"
-          fontWeight="bold"
-          fontSize={"1.1rem"}
-        >
-          {props.title}
-        </Text>
-        <Flex gap=".7rem" className="videoInfo">
-          {/*<Avatar  className="videoAvatar"src={props.avatar} />*/}
+    <Link to={"/playback"} className={"hover:bg-fuchsia-300"}>
+      <Card
+        className={"bg-white hover:bg-fuchsia-300"}
+        variation={"elevated"}
+        width={"350px"}
+        height={"250px"}
+        border={"1px solid gray"}
+      >
+        <Image
+          className={"videoThumbnail"}
+          src={props.thumbnail}
+          alt={props.title}
+        />
+        <View>
           <Text
-            className="videoChannelName"
             isTruncated={true}
-            fontStyle={"italic"}
+            className={"videoTitle"}
+            fontWeight={"bold"}
+            fontSize={"1.1rem"}
           >
-            {props.channelName}
+            {props.title}
           </Text>
-          <Text className="videoData" paddingLeft={"1rem"}>
-            {props.views} views : {props.time}
-          </Text>
-        </Flex>
-      </View>
-    </Card>
+          <Flex gap={".7rem"} className={"videoInfo"}>
+            {/*<Avatar  className="videoAvatar"src={props.avatar} />*/}
+            <Text
+              className="videoChannelName"
+              isTruncated={true}
+              fontStyle={"italic"}
+            >
+              {props.channelName}
+            </Text>
+            <Text className="videoData" paddingLeft={"1rem"}>
+              {props.views} views : {props.time}
+            </Text>
+          </Flex>
+        </View>
+      </Card>
+    </Link>
   );
 }
 
