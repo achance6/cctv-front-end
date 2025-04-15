0e3d3a6129312df585f5db54c24eeb077ec430ca
camerqn <cameronconnelly@ymail.com>
2025-04-15 12:48:40 -0400

    Added title and description components on playback page.

--- src/app/routes/playbackPage.tsx
+++ src/app/routes/playbackPage.tsx
@@ -31,6 +31,8 @@ function PlaybackPage() {
   const [ultraLowResPresignedUrl, setUltraLowResPresignedUrl] = useState<
     string | undefined
   >(undefined);
+  const [description, setDescription] = useState("Description");
+  const [title, setTitle] = useState("Title");
   const [searchParams] = useSearchParams();
 
   useEffect(() => {
