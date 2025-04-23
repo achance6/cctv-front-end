54d3ab768d1c9a61dd672bfda740157b7e6f8244
achance6 <ayden.chance@rocketmail.com>
2025-04-23 16:21:25 -0400

    cleanup

--- src/app/routes/errorSearchPage.tsx
+++ src/app/routes/errorSearchPage.tsx
@@ -1,20 +1,18 @@
 import NavBar from "@/components/navBar";
 import { useSearchParams } from "react-router";
-//import { useParams } from "react-router-dom";
+import { Heading, View } from "@aws-amplify/ui-react";
 
 const ErrorSearchPage = () => {
-  //const { query } = useParams<{ query: string }>();
   const [searchParams, setSearchQuery] = useSearchParams();
   setSearchQuery(searchParams);
 
   return (
-    <div>
-        <NavBar />
-      <h1 className="text-4xl text-center mt-8">
+    <View>
+      <NavBar />
+      <Heading level={1} className="text-4xl text-center mt-8">
         Error! No videos found for this search term {searchParams.get("query")}
-        </h1>
-
-    </div>
+      </Heading>
+    </View>
   );
 };
 
--- src/app/routes/searchPage.tsx
+++ src/app/routes/searchPage.tsx
@@ -1,21 +1,19 @@
-
 import NavBar from "@/components/navBar";
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router";
+import { Heading, View } from "@aws-amplify/ui-react";
 
 const SearchPage = () => {
-  const params = useParams()
-  const query = params.query; // Retrieve the query parameter from the URL  
+  const params = useParams();
+  const query = params.query; // Retrieve the query parameter from the URL
   console.log("Query parameter:", query); // Log the query parameter to the console
 
   return (
-    <div>
+    <View>
       <NavBar />
-      <h1 className="text-4xl text-center mt-8">Search Page</h1>
-      <h1 className="text-4xl text-center mt-8">
-        
-      </h1>
-      
-    </div>
+      <Heading level={1} className="text-4xl text-center mt-8">
+        Search Page
+      </Heading>
+    </View>
   );
 };
 
